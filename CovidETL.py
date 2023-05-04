fb04e40660b51b29ad9706bf582b958292961125
pandeyvivak25@hotmail.com <pandeyvivak25@hotmail.com>
2023-05-04 17:57:32 +0000

    display data perform
    data is loaded in the data lake to databricks

--- CovidETL.py
+++ CovidETL.py
@@ -1,5 +1,22 @@
 # Databricks notebook source
-print('ddd')
+# DBTITLE 1,#Accessing the Azure data lake repo
+acc_key = dbutils.secrets.get(scope="interactiveDV", key="accessKey")
+spark.conf.set("fs.azure.account.key.interactive0d0v.dfs.core.windows.net", acc_key)
+
+
+# COMMAND ----------
+
+# DBTITLE 1,Reading the csv
+df=spark.read.format('csv').option('header', "True").load('abfss://covid-data-raw@interactive0d0v.dfs.core.windows.net/covid_vaccine_statewise.csv')
+
+# COMMAND ----------
+
+# DBTITLE 1,Displaying Dataframe
+display(df)
+
+# COMMAND ----------
+
+
 
 # COMMAND ----------
 
